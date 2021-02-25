2ef5f1a2d2a72cdd2354da64e716636e278f0191
pratamawijaya <set.mnemonix@gmail.com>
2021-02-25 12:23:40 +0700

    sample lazy list with coil

--- Layouting/app/src/main/java/com/pratamawijaya/layouting/MainActivity.kt
+++ Layouting/app/src/main/java/com/pratamawijaya/layouting/MainActivity.kt
@@ -3,15 +3,26 @@ package com.pratamawijaya.layouting
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.pratamawijaya.layouting.ui.theme.LayoutingTheme
+import dev.chrisbanes.accompanist.coil.CoilImage
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,15 +37,92 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun LayoutCodelabs() {
-    Scaffold { innerPadding ->
-        Text(text = "Hi there", modifier = Modifier.padding(innerPadding))
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Sample Top Bar")
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = "favorite")
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        BodyContent(
+            Modifier
+                .padding(innerPadding)
+                .padding(8.dp)
+        )
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun LazyList() {
+    val scrollState = rememberScrollState()
+
+    LazyColumn(Modifier.verticalScroll(scrollState)) {
+        items(100) {
+            Text("Item #$it")
+        }
+    }
 }
+
+@Composable
+fun ImageList(count: Int, scrollState: LazyListState) {
+    LazyColumn(state = scrollState) {
+        items(count) {
+            ImageListItem(it)
+        }
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        CoilImage(
+            data = "https://developer.android.com/images/brand/Android_Robot.png",
+            contentDescription = "",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text("Item $index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    val listSize = 100
+    val scrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text("Scroll to the top")
+            }
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+                Text("Scroll to the end")
+            }
+        }
+
+        ImageList(
+            count = listSize,
+            scrollState = scrollState
+        )
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
