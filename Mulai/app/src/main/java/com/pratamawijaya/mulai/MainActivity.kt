ea1e8c4b3e80c0fb6af3e518eb9cbe5f37d4f110
pratamawijaya <set.mnemonix@gmail.com>
2021-02-25 10:50:17 +0700

    sample composable function with param

--- Mulai/app/src/main/java/com/pratamawijaya/mulai/MainActivity.kt
+++ Mulai/app/src/main/java/com/pratamawijaya/mulai/MainActivity.kt
@@ -7,32 +7,36 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.pratamawijaya.mulai.ui.components.Greeting
 import com.pratamawijaya.mulai.ui.theme.MulaiTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MulaiTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp {
+                Greeting(name = "Hello")
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(content: @Composable () -> Unit) {
+    MulaiTheme() {
+        Surface(color = Color.Yellow) {
+            content()
+        }
+    }
 }
 
-@Preview(showBackground = true)
+
+@Preview("Text Preview")
 @Composable
-fun DefaultPreview() {
-    MulaiTheme {
-        Greeting("Android")
+fun DefaultPreview(){
+    MyApp {
+        Greeting(name = "Hello")
     }
 }
---
+++ Mulai/app/src/main/java/com/pratamawijaya/mulai/ui/components/Greeting.kt
@@ -0,0 +1,26 @@
+package com.pratamawijaya.mulai.ui.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.pratamawijaya.mulai.ui.theme.MulaiTheme
+
+@Composable
+fun Greeting(name: String) {
+    Surface(color = Color.Yellow) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MulaiTheme {
+        Greeting("Android")
+    }
+}
