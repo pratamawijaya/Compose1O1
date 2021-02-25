1c1b34b8975b5bf1cae5df3bcc94154f8499ccb1
pratamawijaya <set.mnemonix@gmail.com>
2021-02-25 11:08:53 +0700

    sample handle state

--- Mulai/app/src/main/java/com/pratamawijaya/mulai/MainActivity.kt
+++ Mulai/app/src/main/java/com/pratamawijaya/mulai/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.pratamawijaya.mulai.ui.components.Greeting
 import com.pratamawijaya.mulai.ui.theme.MulaiTheme
 
@@ -27,11 +27,27 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+    val counterState = remember { mutableStateOf(0) }
+
     Column {
         for (name in names) {
             Greeting(name = name)
             Divider(color = Color.Black)
-        };
+        }
+        Divider(color = Color.Transparent, thickness = 32.dp)
+        Counter(
+            count = counterState.value,
+            updateCount = { newCount ->
+                counterState.value = newCount
+            }
+        )
+    }
+}
+
+@Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
+    Button(onClick = { updateCount(count + 1) }) {
+        Text("I've been click $count times")
     }
 }
 
