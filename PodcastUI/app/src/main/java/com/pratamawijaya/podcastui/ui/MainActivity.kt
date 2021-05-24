3a3cb48dcfbb86a33efac5516952415de6fd8689
pratamawijaya <set.mnemonix@gmail.com>
2021-05-24 15:37:10 +0700

    setup color theme, finish welcome screen

--- PodcastUI/app/src/main/java/com/pratamawijaya/podcastui/ui/MainActivity.kt
+++ PodcastUI/app/src/main/java/com/pratamawijaya/podcastui/ui/MainActivity.kt
@@ -2,37 +2,69 @@ package com.pratamawijaya.podcastui.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedDispatcher
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navDeepLink
+import com.pratamawijaya.podcastui.R
+import com.pratamawijaya.podcastui.ui.navigation.Destination
+import com.pratamawijaya.podcastui.ui.navigation.Navigator
+import com.pratamawijaya.podcastui.ui.navigation.ProvideNavHostController
 import com.pratamawijaya.podcastui.ui.theme.PodcastUITheme
+import com.pratamawijaya.podcastui.ui.welcome.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setTheme(R.style.Theme_PodcastUI)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        val startDestination = Destination.welcome
+
         setContent {
-            PodcastUITheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            PodcastUiApp(
+                backDispatcher = onBackPressedDispatcher,
+                startDestination = startDestination
+            )
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
+fun PodcastUiApp(
+    startDestination: String = Destination.welcome,
+    backDispatcher: OnBackPressedDispatcher
+) {
     PodcastUITheme {
-        Greeting("Android")
+        ProvideNavHostController {
+            Box(modifier = Modifier.fillMaxSize()) {
+                NavHost(Navigator.current, startDestination) {
+                    composable(Destination.welcome) {
+                        WelcomeScreen()
+                    }
+
+                    composable(Destination.home) {
+                        // todo
+                    }
+
+                    composable(
+                        Destination.podcast,
+                        deepLinks = listOf(navDeepLink {
+                            uriPattern = "https://anu.com/e/{id}"
+                        })
+                    ) { backStackEntry ->
+                        // todo
+                    }
+
+                }
+            }
+        }
     }
 }
--- PodcastUI/app/src/main/java/com/pratamawijaya/podcastui/ui/welcome/WelcomeScreen.kt
+++ PodcastUI/app/src/main/java/com/pratamawijaya/podcastui/ui/welcome/WelcomeScreen.kt
@@ -2,14 +2,20 @@ package com.pratamawijaya.podcastui.ui.welcome
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Surface
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.navigate
-import androidx.navigation.compose.popUpTo
+import androidx.compose.ui.unit.dp
 import com.pratamawijaya.podcastui.ui.common.PreviewContent
 import com.pratamawijaya.podcastui.ui.navigation.Destination
 import com.pratamawijaya.podcastui.ui.navigation.Navigator
@@ -43,6 +49,9 @@ fun WelcomeScreenContent(visible: Boolean, onGetStarted: () -> Unit) {
 
             AnimatedImage(visible = visible)
 
+            Spacer(modifier = Modifier.height(16.dp))
+
+            AnimatedButton(visible = visible, onClick = onGetStarted)
 
         }
     }
