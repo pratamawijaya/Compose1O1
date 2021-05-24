af7289fe4a7766513766986af4a1b4ed3c738f22
pratamawijaya <set.mnemonix@gmail.com>
2021-05-24 14:47:48 +0700

    setup nunito font

--- PodcastUI/app/src/main/java/com/pratamawijaya/podcastui/ui/theme/Type.kt
+++ PodcastUI/app/src/main/java/com/pratamawijaya/podcastui/ui/theme/Type.kt
@@ -2,9 +2,18 @@ package com.pratamawijaya.podcastui.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.pratamawijaya.podcastui.R
+
+val NunitoSans = FontFamily(
+    Font(R.font.nunito_sans_regular),
+    Font(R.font.nunito_sans_light, weight = FontWeight.Light),
+    Font(R.font.nunito_sans_bold, weight = FontWeight.Bold),
+    Font(R.font.nunito_sans_semibold, weight = FontWeight.SemiBold)
+)
 
 
 // Set of Material typography styles to start with
