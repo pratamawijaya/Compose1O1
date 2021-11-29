924474a47cca95e16d48febfe27c99307bf5482f
pratamawijaya <set.mnemonix@gmail.com>
2021-11-29 11:10:54 +0700

    add state for button

--- compose_01_basic/app/src/main/java/com/pratamawijaya/basic/MainActivity.kt
+++ compose_01_basic/app/src/main/java/com/pratamawijaya/basic/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,18 +39,26 @@ private fun MyApp(names: List<String> = listOf("World", "Compose")) {
 
 @Composable
 fun Greeting(name: String) {
+    var expanded = remember {
+        mutableStateOf(false)
+    }
+
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier
-            .padding(24.dp)) {
+        Row(
+            modifier = Modifier
+                .padding(24.dp)
+        ) {
             Column(modifier = Modifier.weight(1f)) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
-            OutlinedButton(onClick = {}) {
-                Text(text = "Show More")
+            OutlinedButton(onClick = {
+                expanded.value = !expanded.value
+            }) {
+                Text(text = if(expanded.value) "Show Less" else "Show More")
             }
         }
 
